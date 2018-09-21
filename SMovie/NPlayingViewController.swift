5ee155464c1a826f7af02e9bdf085133e0a3a7cf
LysAMa <ladouceurlyse44@gmail.com>
2018-09-20 23:40:53 -0400

    Table View Setup

--- SMovie/NPlayingViewController.swift
+++ SMovie/NPlayingViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class NPlayingViewController: UIViewController {
+class NPlayingViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var MovieTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        MovieTableView.dataSource = self
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -39,8 +41,14 @@ class NPlayingViewController: UIViewController {
     }
     
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
     
-    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+            return cell
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
