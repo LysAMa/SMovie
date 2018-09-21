ed199ba25719e9ca2c940617cfd106f9cd787a98
LysAMa <ladouceurlyse44@gmail.com>
2018-09-21 01:55:00 -0400

    Pull to Refresh

--- SMovie/NPlayingViewController.swift
+++ SMovie/NPlayingViewController.swift
@@ -14,31 +14,42 @@ class NPlayingViewController: UIViewController, UITableViewDataSource {
     @IBOutlet weak var MovieTableView: UITableView!
     
     var movies: [[String: Any]] = []
+    var refreshControl: UIRefreshControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(NPlayingViewController.didPullToRefresh(_:)), for: .valueChanged)
+        MovieTableView.insertSubview(refreshControl, at: 0)
         
         MovieTableView.dataSource = self
-        
         MovieTableView.rowHeight = 250
         MovieTableView.estimatedRowHeight = 250
         
-        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
-        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
-        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-        let task = session.dataTask(with: request) { (data, response, error) in
+        fetchNowPlayingMovie()
+    }
+    
+    @objc func didPullToRefresh(_ refreshControl: UIRefreshControl){
+        fetchNowPlayingMovie()
+    }
+    
+    func fetchNowPlayingMovie(){
+            let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+            let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+            let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+            let task = session.dataTask(with: request) { (data, response, error) in
             // This will run when the network request returns
             if let error = error {
-                print(error.localizedDescription)
+            print(error.localizedDescription)
             } else if let data = data {
-                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-                let movies = dataDictionary["results"] as! [[String: Any]]
-                self.movies = movies
-                self.MovieTableView.reloadData()
+            let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+            let movies = dataDictionary["results"] as! [[String: Any]]
+            self.movies = movies
+            self.MovieTableView.reloadData()
+            self.refreshControl.endRefreshing()
             }
         }
         task.resume()
-       
     }
     
     
