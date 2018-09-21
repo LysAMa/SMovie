615d0862aec152b07efc5a82271b700d587c25ca
LysAMa <ladouceurlyse44@gmail.com>
2018-09-21 00:30:34 -0400

    TableView + Data

---
+++ SMovie/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  SMovie
+//
+//  Created by MacBookPro10 on 9/21/18.
+//  Copyright © 2018 MacBookPro10. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var TitleLabel: UILabel!
+    
+    @IBOutlet weak var OverviewLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- SMovie/NPlayingViewController.swift
+++ SMovie/NPlayingViewController.swift
@@ -11,10 +11,17 @@ import UIKit
 class NPlayingViewController: UIViewController, UITableViewDataSource {
 
     @IBOutlet weak var MovieTableView: UITableView!
+    
+    var movies: [[String: Any]] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         MovieTableView.dataSource = self
+        
+        MovieTableView.rowHeight = 250
+        MovieTableView.estimatedRowHeight = 250
+        
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -25,15 +32,8 @@ class NPlayingViewController: UIViewController, UITableViewDataSource {
             } else if let data = data {
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                 let movies = dataDictionary["results"] as! [[String: Any]]
-                for movie in movies{
-                    let title = movie["title"] as! String
-                    print(title)
-                }
-                
-                // TODO: Get the array of movies
-                // TODO: Store the movies in a property to use elsewhere
-                // TODO: Reload your table view data
-                
+                self.movies = movies
+                self.MovieTableView.reloadData()
             }
         }
         task.resume()
@@ -42,12 +42,19 @@ class NPlayingViewController: UIViewController, UITableViewDataSource {
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return movies.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
-            return cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
+        
+        let movie = movies[indexPath.row]
+        let title = movie["title"] as! String
+        let overview = movie["overview"] as! String
+        cell.TitleLabel.text = title
+        cell.OverviewLabel.text = overview
+        
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
